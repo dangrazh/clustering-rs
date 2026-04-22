9e10226e3dd310461a898175937347817ee27108
dangrazh <dani.grass@bluewin.ch>
2026-04-22 21:18:45 +0200

    no console in release mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 use anyhow::Result;
 use eframe::egui;
 use incident_clustering_analyzer::app::IncidentClusteringApp;
